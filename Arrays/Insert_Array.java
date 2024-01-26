a47af1fbd0f8e4d1d3818c194930dc4ad210f1ec
DnyaneshKshatriya07 <dnyaneshkshatriya7123@gmail.com>
2024-01-26 11:03:36 +0530

    Insert Array

---
+++ Arrays/Insert_Array.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Insert_Array {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the size of Array:- ");
+        Scanner sc =new Scanner(System.in);
+        int n= sc.nextInt();
+        
+        int i;
+        int arr[] =new int[n];
+        System.out.println("Enter the the values:- ");
+        for(i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        
+
+        System.out.println("Initial Array:\n" + Arrays.toString(arr));
+
+        System.out.println("Enter the value to insert:- ");
+        int value=sc.nextInt();
+        
+        System.out.println("Enter the position where you want to insert the element:- ");
+        int position=sc.nextInt();
+
+        int newarr[] = new int[n + 1];
+
+        for (i = 0; i < n + 1; i++) {
+            if (i < position - 1)
+                newarr[i] = arr[i];
+            else if (i == position - 1)
+                newarr[i] = value;
+            else
+                newarr[i] = arr[i - 1];
+        }
+        arr = newarr;
+
+        System.out.println("\nArray with " + value + " inserted at positionition " + position + ":\n" + Arrays.toString(arr));
+    }
+}
